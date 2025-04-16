748dc35587976d1493d873f33c9b58e333a2978c
Vuthao1603 <vuvanthao1603@gmail.com>
2025-04-16 17:39:52 +0700

    fix login registerr

--- src/index.js
+++ src/index.js
@@ -30,6 +30,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
